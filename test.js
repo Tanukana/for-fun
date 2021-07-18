2f5c204c177f7c27341495610b77dc8bb1511457
Tony W <tonywu.fs@gmail.com>
2021-07-17 19:57:19 -0700

    partial solution to fibonacci exercise

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-let sent = "Hello, World";
-
-let newSent = sent.replace(/[' ',]/g, "");
+const fibArr = [1, 1];
+    let a = 0;
+    let b = 1;
+    let fibSum = 0;
+    function addFib() {
+        fibSum = fibArr[a] + fibArr[b];
+        fibArr.push(fibSum);
+        a++;
+        b++;
+    }
